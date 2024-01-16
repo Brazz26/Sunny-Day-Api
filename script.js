7bf70e1c5de43d759c780b3ee78c6eb453a3e81c
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2024-01-16 13:09:24 -0500

    created 5day cards with function, history functionality, localstorage, and save btn

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const apiKey = '695ba21512908372e18bcecc3edd916f'
 const searchBox = document.querySelector('#search')
 const searchBtn = document.querySelector('#search-btn')
 const dailyWeather = document.querySelector('.today')
-
+const fiveDayContainer = document.querySelector('.five-Day')
+const historyContainer = document.querySelector('.history-btn')
 
 async function search(searchCity) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=imperial&appid=${apiKey}`)
-        
-        const data = await response.json() 
+
+        const data = await response.json()
 
         const weatherIcon = data.weather[0].icon
         const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}.png`
@@ -23,27 +24,71 @@ async function search(searchCity) {
         <img src='${iconUrl}'/>
         </div>
         `;
-        dailyWeather.innerHTML = todayWeather 
-        
+        dailyWeather.innerHTML = todayWeather
+
         const lat = data.coord.lat
         const lon = data.coord.lon
         try {
-            // const fiveDayResponse = await fetch(`https://openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
-            const fiveDayResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`);
-            const fiveDayData = await fiveDayResponse.json()
-            const formattedData = fiveDayData.list.filter(day => day.dt_txt.includes('12:00:00'));
-            console.log(formattedData)
-        } catch (error) {
-            console.error(error)
+            const fiveDayResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        const fiveDayData = await fiveDayResponse.json()
+        const formattedData = fiveDayData.list.filter(day => day.dt_txt.includes('12:00:00'));
+        
+            let weeklyCard = '';
+        for(var i = 0; i < formattedData.length; i++){
+            console.log(formattedData[i])
+
+            const dayOfWeek = new Date(formattedData[i].dt_txt.toLocaleString());
+            const icon = formattedData[i].weather[0].icon
+            const iconImg = `<img src = 'https://openweathermap.org/img/wn/${icon}.png'/>`
+            weeklyCard += `
+            <div class='card'>
+            <p>${dayOfWeek}</p>
+            ${iconImg}
+            <h3>Temperature ${formattedData[i].main.temp}</h3>
+            <h3>Humidity ${formattedData[i].main.humidity}</h3>
+            <h3>Wind Speed ${formattedData[i].wind.speed}</h3>
+            </div>
+            `;
+            
+            fiveDayContainer.innerHTML = weeklyCard
+            console.log(dayOfWeek)
         }
     } catch (error) {
         console.error(error)
     }
 }
+catch (error) {
+    console.error(error)
+}
+}
 
 
-searchBtn.addEventListener('click', function(event){
+searchBtn.addEventListener('click', function (event) {
     event.preventDefault()
     const searchCity = searchBox.value.trim()
+    saveSearches()
     search(searchCity)
 })
+function saveSearches() {
+    let previousSearch = searchBox.value.trim()
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+    history.push(previousSearch)
+    localStorage.setItem('history', JSON.stringify(history))
+    createBtn(history)
+}
+
+function createBtn(history) {
+    for(var i = 0; i < history.length; i++) {
+        const btn = document.createElement('li')
+        btn.textContent = history[i]
+        btn.className += 'city-btn'
+        historyContainer.appendChild(btn)
+
+        btn.addEventListener('click', function(event){
+            event.preventDefault()
+            let pastCity = btn.textContent
+            search(pastCity)
+        })
+    }
+
+}
